5764e22514e4293b4f91850dc45ec63ffe9df607
meetp1211 <meet.160410116094@gmail.com>
2024-07-15 03:15:47 -0400

    Design Applicant page and schedule page

--- src/container/ViewApplicationEmployer/ApplicantDetails.jsx
+++ src/container/ViewApplicationEmployer/ApplicantDetails.jsx
@@ -1,34 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Button, Card, TextField, IconButton, Snackbar, Alert } from '@mui/material';
-import { Download } from '@mui/icons-material';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  Button,
+  Card,
+  TextField,
+  IconButton,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import { Download } from "@mui/icons-material";
+import axios from "axios";
+import "../ViewApplicationEmployer/ApplicantDetails.css";
+import Navbar from "../../components/Common/Navbar";
+import { textAlign } from "@mui/system";
+import Footer from "../../components/Common/Footer";
 
-const ApplicantDetails = ({ primaryColor, cardColor }) => {
+
+const ApplicantDetails = () => {
+  const [primaryColor, setPrimaryColor] = useState("");
+  const [primaryFontColor, setPrimaryFontColor] = useState("");
+  const [secondaryFontColor, setSecondaryFontColor] = useState("");
+  const [cardColor, setcardColor] = useState("");
+  const [footerLinkColor, setfooterLinkColor] = useState("");
+
   const { id } = useParams();
   const navigate = useNavigate();
   const [applicantDetails, setApplicantDetails] = useState(null);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
 
-
   useEffect(() => {
+    const rootStyles = getComputedStyle(document.documentElement);
+    setPrimaryColor(rootStyles.getPropertyValue("--primary-color").trim());
+    setPrimaryFontColor(
+      rootStyles.getPropertyValue("--primary-font-color").trim()
+    );
+    setSecondaryFontColor(
+      rootStyles.getPropertyValue("--secondary-font-color").trim()
+    );
+    setcardColor(rootStyles.getPropertyValue("--card-color").trim());
+    setfooterLinkColor(
+      rootStyles.getPropertyValue("--footer-link-color").trim()
+    );
+
     const fetchApplicantDetails = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/jobapplications/jobapplication/${id}`, {
-          headers: {
-            "x-auth-token": sessionStorage.getItem("user")
+        const response = await axios.get(
+          `http://localhost:5000/api/jobapplications/jobapplication/${id}`,
+          {
+            headers: {
+              "x-auth-token": sessionStorage.getItem("user"),
+            },
           }
-        });
+        );
         setApplicantDetails(response.data);
       } catch (error) {
-        console.error('Error fetching applicant details', error);
+        console.error("Error fetching applicant details", error);
       }
     };
 
     fetchApplicantDetails();
   }, [id]);
 
-  
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
   };
@@ -39,14 +71,18 @@ const ApplicantDetails = ({ primaryColor, cardColor }) => {
 
   const handleRejectApplication = async () => {
     try {
-      await axios.put(`http://localhost:5000/api/jobapplications/rejectapplication/${id}`, {}, {
-        headers: {
-          "x-auth-token": sessionStorage.getItem("user")
+      await axios.put(
+        `http://localhost:5000/api/jobapplications/rejectapplication/${id}`,
+        {},
+        {
+          headers: {
+            "x-auth-token": sessionStorage.getItem("user"),
+          },
         }
-      });
+      );
       setSnackbarOpen(true);
     } catch (error) {
-      console.error('Error rejecting application', error);
+      console.error("Error rejecting application", error);
     }
   };
 
@@ -55,64 +91,189 @@ const ApplicantDetails = ({ primaryColor, cardColor }) => {
   }
 
   return (
-    <div className="container-fluid my-2" style={{ padding: '20px' }}>
+    <div className="">
       <div className="row">
+      <Navbar
+            logo="/logo.png"
+            primaryFontColor={primaryFontColor}
+            primaryColor={primaryColor}
+          />
         <div className="col-md-12 col-sm-12">
-          <div className="text-center">
-            <h2 className="" style={{ color: primaryColor }}>
+          
+          <div className="applicant-heding" style={{color: primaryFontColor, backgroundColor: primaryColor}}>
+            <h1>Candidate Application</h1>
+            <p>Review and manage applications from potential candidates</p>
+          </div>
+          <div className="text-center applicant-details">
+            <h2
+              className=""
+              style={{ color: primaryColor, textAlign: "center" , margin:"50px" }}
+            >
               Job Role: {applicantDetails.job_id.title}
             </h2>
-            <Card
-              style={{ background: cardColor, padding: '1rem', borderRadius: '15px', textAlign: 'center', position: 'relative', cursor: 'pointer', marginTop: '10px' }}
+            <div
+              style={{
+                background: primaryFontColor,
+                padding: "1rem",
+                borderRadius: "15px",
+                textAlign: "center",
+                position: "relative",
+                cursor: "pointer",
+                margin: "50px",
+              }}
             >
-              <h5 className="card-title" style={{ fontWeight: 'bold', marginBottom: '16px' }}>
-                {applicantDetails.user_id.name}
-              </h5>
-              <TextField fullWidth variant="outlined" label="First Name" value={applicantDetails.firstName} style={{ marginBottom: '16px' }} disabled />
-              <TextField fullWidth variant="outlined" label="Last Name" value={applicantDetails.lastName} style={{ marginBottom: '16px' }} disabled />
-              <TextField fullWidth variant="outlined" label="Email" value={applicantDetails.email} style={{ marginBottom: '16px' }} disabled />
-              <TextField fullWidth variant="outlined" label="Phone Number" value={applicantDetails.phoneNumber} style={{ marginBottom: '16px' }} disabled />
+              <h3
+                className="card-title"
+                style={{
+                  fontWeight: "bold",
+                  margin:"15px",
+                  color: primaryColor,
 
-              <div style={{ marginBottom: '16px' }}>
-                <a href={`http://localhost:5000/uploads/${applicantDetails.resume.split('\\').pop()}`} download>
-                  <IconButton color="primary">
-                    <Download />
-                  </IconButton>
-                  Resume
-                </a>
+                }}
+              >
+                {/* {applicantDetails.user_id.name} */}
+                Personal Details
+              </h3>
+              <div className="personal-details " style={{}}>
+                <TextField
+                  type="text"
+                  id="outlined-basic"
+                  label="First Name"
+                  variant="outlined"
+                  name="First Name"
+                  className="login-field"
+                  value={applicantDetails.firstName}
+                  style={{ marginBottom: "16px" }}
+                  disabled
+                />
+                <TextField
+                  type="text"
+                  id="outlined-basic"
+                  label="Last Name"
+                  variant="outlined"
+                  name="Last Name"
+                  className="login-field"
+                  value={applicantDetails.lastName}
+                  style={{ marginBottom: "16px" }}
+                  disabled
+                />
               </div>
-              <div style={{ marginBottom: '16px' }}>
-                <a href={`http://localhost:5000/uploads/${applicantDetails.cover_letter.split('\\').pop()}`} download>
-                  <IconButton color="primary">
-                    <Download />
-                  </IconButton>
-                  Cover Letter
-                </a>
-              </div>
-              <div style={{ marginBottom: '16px' }}>
-                <a href={`http://localhost:5000/uploads/${applicantDetails.portfolio.split('\\').pop()}`} download>
-                  <IconButton color="primary">
-                    <Download />
-                  </IconButton>
-                  Portfolio
-                </a>
+              <div className="personal-details">
+                <TextField
+                  type="text"
+                  id="outlined-basic"
+                  variant="outlined"
+                  name="First Name"
+                  className="login-field"
+                  label="Email"
+                  value={applicantDetails.email}
+                  style={{ marginBottom: "16px" }}
+                  disabled
+                />
+                <TextField
+                  type="text"
+                  id="outlined-basic"
+                  variant="outlined"
+                  name="First Name"
+                  className="login-field"
+                  label="Phone number"
+                  value={applicantDetails.phoneNumber}
+                  style={{ marginBottom: "16px" }}
+                  disabled
+                />
               </div>
 
-              <Button variant="contained" style={{ backgroundColor: 'blue', color: 'white', marginRight: '10px' }} onClick={handleScheduleInterview}>
+              <div className="download-items">
+                <div style={{ marginBottom: "16px" }}>
+                  <a
+                    href={`http://localhost:5000/uploads/${applicantDetails.resume
+                      .split("\\")
+                      .pop()}`}
+                    download
+                  >
+                    <IconButton color="primary">
+                      <Download />
+                    </IconButton>
+                    Resume
+                  </a>
+                </div>
+                <div style={{ marginBottom: "16px" }}>
+                  <a
+                    href={`http://localhost:5000/uploads/${applicantDetails.cover_letter
+                      .split("\\")
+                      .pop()}`}
+                    download
+                  >
+                    <IconButton color="primary">
+                      <Download />
+                    </IconButton>
+                    Cover Letter
+                  </a>
+                </div>
+                <div style={{ marginBottom: "16px" }}>
+                  <a
+                    href={`http://localhost:5000/uploads/${applicantDetails.portfolio
+                      .split("\\")
+                      .pop()}`}
+                    download
+                  >
+                    <IconButton color="primary">
+                      <Download />
+                    </IconButton>
+                    Portfolio
+                  </a>
+                </div>
+              </div>
+
+              <button
+                variant="contained"
+                style={{
+                  backgroundColor: primaryColor,
+                  color: primaryFontColor,
+                  padding: "12px",
+                  borderRadius: "10px",
+                  border: "1px solid",
+                  margin: "30px",
+                }}
+                onClick={handleScheduleInterview}
+              >
                 Schedule Interview
-              </Button>
-              <Button variant="contained" style={{ backgroundColor: 'purple', color: 'white' }} onClick={handleRejectApplication}>
+              </button>
+              <button
+                variant="contained"
+                style={{
+                  backgroundColor: primaryFontColor,
+                  color: primaryColor,
+                  padding: "12px",
+                  borderRadius: "10px",
+                  border: "1px solid",
+                }}
+                onClick={handleRejectApplication}
+              >
                 Reject Application
-              </Button>
-            </Card>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
-        <Alert onClose={handleSnackbarClose} severity="error" sx={{ width: '100%' }}>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
           Application Rejected
         </Alert>
       </Snackbar>
+      <Footer
+              PrimaryColor={primaryColor}
+              PrimaryFontColor={primaryFontColor}
+              FooterLinkColor={footerLinkColor}
+           />
     </div>
   );
 };
--- src/container/ViewApplicationEmployer/ScheduleInterview.jsx
+++ src/container/ViewApplicationEmployer/ScheduleInterview.jsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Button, Card, TextField, Typography, Snackbar, Alert } from '@mui/material';
 import axios from 'axios';
+import "../ViewApplicationEmployer/ApplicantDetails.css";
+import Navbar from '../../components/Common/Navbar';
+import Footer from '../../components/Common/Footer';
 
-const ScheduleInterview = ({ primaryColor, cardColor }) => {
+const ScheduleInterview = () => {
+
+  const [primaryColor, setPrimaryColor] = useState("");
+  const [primaryFontColor, setPrimaryFontColor] = useState("");
+  const [secondaryFontColor, setSecondaryFontColor] = useState("");
+  const [cardColor, setcardColor] = useState("");
+  const [footerLinkColor, setfooterLinkColor] = useState("");
+
+  useEffect(() => {
+    // Fetch the CSS variables after component mounts
+    const rootStyles = getComputedStyle(document.documentElement);
+    setPrimaryColor(rootStyles.getPropertyValue("--primary-color").trim());
+    setPrimaryFontColor(
+      rootStyles.getPropertyValue("--primary-font-color").trim()
+    );
+    setSecondaryFontColor(
+      rootStyles.getPropertyValue("--secondary-font-color").trim()
+    );
+    setcardColor(rootStyles.getPropertyValue("--card-color").trim());
+    setfooterLinkColor(
+      rootStyles.getPropertyValue("--footer-link-color").trim()
+    );
+
+  }, []);
+
   const { id } = useParams();
   const navigate = useNavigate();
   const [interviewDates, setInterviewDates] = useState([{ date: '', time: '' }, { date: '', time: '' }, { date: '', time: '' }]);
@@ -48,20 +75,37 @@ const ScheduleInterview = ({ primaryColor, cardColor }) => {
   };
 
   return (
-    <div className="container-fluid my-2" style={{ padding: '20px' }}>
+    <div className="">
       <div className="row">
-        <div className="col-md-12 col-sm-12">
+      <Navbar
+                logo="/logo.png"
+                primaryFontColor={primaryFontColor}
+                primaryColor={primaryColor}
+            />
+        <div className="col-md-12 col-sm-12" style={{padding:"0px"}}>
+       
+        <div className="applicant-heding" style={{color: primaryFontColor, backgroundColor: primaryColor}}>
+            <h1>Candidate Application</h1>
+            <p>Review and manage applications from potential candidates</p>
+          </div>
           <div className="text-center">
-            <Typography variant="h4" style={{ color: primaryColor }}>Schedule Interview</Typography>
-            <Card style={{ background: cardColor, padding: '2rem', borderRadius: '15px', textAlign: 'center', position: 'relative', marginTop: '10px' }}>
+            {/* <Typography variant="h4" style={{ color: primaryColor }}>Schedule Interview</Typography> */}
+            <h2
+              className=""
+              style={{ color: primaryColor, textAlign: "center", margin:"40px" }}
+            >
+              {/* Job Role: {applicantDetails.job_id.title} */}
+              Schedule Interview
+            </h2>
+            <div className='download-items' style={{ background: primaryFontColor, borderRadius: '15px', textAlign: 'center', }}>
               {interviewDates.map((slot, index) => (
-                <div key={index} style={{ marginBottom: '16px' }}>
+                <div key={index} style={{ margin: '20px'}}>
                   <TextField
                     label={`Date Option ${index + 1}`}
                     type="date"
                     value={slot.date}
                     onChange={(e) => handleSlotChange(index, 'date', e.target.value)}
-                    style={{ marginRight: '8px', width: '45%' }}
+                    style={{ marginRight: '8px', width: '20%' }}
                     InputLabelProps={{ shrink: true }}
                   />
                   <TextField
@@ -69,20 +113,35 @@ const ScheduleInterview = ({ primaryColor, cardColor }) => {
                     type="time"
                     value={slot.time}
                     onChange={(e) => handleSlotChange(index, 'time', e.target.value)}
-                    style={{ width: '45%' }}
+                    style={{ width: '20%' }}
                     InputLabelProps={{ shrink: true }}
                   />
                 </div>
               ))}
-              <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-                <Button variant="contained" style={{ backgroundColor: 'blue', color: 'white', width: '45%' }} onClick={handleSchedule}>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', alignItems: "center" }}>
+                <button variant="contained" style={{
+                  backgroundColor: primaryColor,
+                  color: primaryFontColor,
+                  padding: "12px",
+                  borderRadius: "10px",
+                  border: "1px solid",
+                  margin: "30px",
+                  width:"250px"
+                }} onClick={handleSchedule}>
                   Confirm
-                </Button>
-                <Button variant="contained" style={{ backgroundColor: 'gray', color: 'white', width: '45%' }} onClick={() => navigate(-1)}>
+                </button>
+                <button variant="contained" style={{
+                  backgroundColor: primaryFontColor,
+                  color: primaryColor,
+                  padding: "12px",
+                  borderRadius: "10px",
+                  border: "1px solid",
+                  width:"250px"
+                }} onClick={() => navigate(-1)}>
                   Cancel
-                </Button>
+                </button>
               </div>
-            </Card>
+            </div>
           </div>
         </div>
       </div>
@@ -91,6 +150,11 @@ const ScheduleInterview = ({ primaryColor, cardColor }) => {
           {snackbarMessage}
         </Alert>
       </Snackbar>
+      <Footer
+              PrimaryColor={primaryColor}
+              PrimaryFontColor={primaryFontColor}
+              FooterLinkColor={footerLinkColor}
+           />
     </div>
   );
 };
